89c22fbe4b6bcd27846cc02fd42cb7a419bddf48
Charlys <johncarlosaguilardark22@gmail.com>
2025-08-21 00:20:38 -0400

    subir tienda

---
+++ store/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Bienvenido a Stor</h1>
+        <p>Tu tienda en línea</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
